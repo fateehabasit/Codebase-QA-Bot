94b111ed11f40815a3ab2518f36e7cedb2777346
fateehabasit <l250775@lhr.nu.edu.pk>
2026-08-11 13:47:57 -0700

    Removed unused chunking.py and test_groq.py

--- src/chunking.py
+++
@@ -1,8 +0,0 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter, Language
-
-splitter = RecursiveCharacterTextSplitter.from_language(
-    language=Language.CPP, chunk_size=800, chunk_overlap=100
-)
-
-def chunk_docs(docs):
-    return splitter.split_documents(docs)
